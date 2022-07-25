543341bde06009e7773ab199578b0db269f387f9
Bao <baokun29082001@gmail.com>
2022-07-25 09:14:37 +0700

    hoan thanh bai 1 2 3 4 5 7

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ function inSoChanLe() {
     }
     console.log(soChan);
     console.log(soLe);
-    document.getElementById("sochanle").innerHTML = "Số chẵn \n" + soChan + 
+    document.getElementById("sochanle").innerHTML = "Số chẵn \n" + soChan +
         "\nSố lẻ \n" + soLe;
-   
+
 }
 document.getElementById("timsochanle").onclick = inSoChanLe;
 
@@ -28,18 +28,18 @@ function chiahetcho3() {
 
 
     var number = 1000;
-    
-    var count =0;
+
+    var count = 0;
     for (var i = 0; i < number; i++) {
         if (i % 3 == 0) {
-            
+
             count++;
-        } 
+        }
     }
     console.log(count);
-    
-    document.getElementById("chia3").innerHTML = "Số chia hết cho 3 nhỏ hơn 1000: "+ "\n" + Number(count);
-   
+
+    document.getElementById("chia3").innerHTML = "Số chia hết cho 3 nhỏ hơn 1000: " + "\n" + Number(count);
+
 }
 document.getElementById("timsochia3").onclick = chiahetcho3;
 
@@ -51,16 +51,16 @@ function timsonguyenduong() {
 
     var S = 0;
     var t = 0;
-    
-   while(S < 10000){
-    t++;
-    S=S+t;  
-   }
-    
+
+    while (S < 10000) {
+        t++;
+        S = S + t;
+    }
+
     console.log(t);
-    
-    document.getElementById("songuyenduong").innerHTML = "Số nguyên dương nhỏ nhất:  "+ "\n" + Number(t);
-   
+
+    document.getElementById("songuyenduong").innerHTML = "Số nguyên dương nhỏ nhất:  " + "\n" + Number(t);
+
 }
 document.getElementById("tinh").onclick = timsonguyenduong;
 
@@ -71,17 +71,17 @@ function timtongluythua() {
 
     var x = document.getElementById("nhapsox").value;
     var n = document.getElementById("nhapson").value;
-    
-    var tong =0;
+
+    var tong = 0;
     var bd = 1;
-    for(i = 1;i <= n;i++){
-        bd =bd*x;
-        tong += bd 
+    for (i = 1; i <= n; i++) {
+        bd = bd * x;
+        tong += bd
     }
- 
-    
-    document.getElementById("tong").innerHTML = "Tổng:  "+ "\n" + Number(tong);
-   
+
+
+    document.getElementById("tong").innerHTML = "Tổng:  " + "\n" + Number(tong);
+
 }
 document.getElementById("tinhtong").onclick = timtongluythua;
 
@@ -90,55 +90,45 @@ document.getElementById("tinhtong").onclick = timtongluythua;
 // bai 5
 function tinhgiaithua() {
 
-    
+
     var giaithua = document.getElementById("nhapsoN").value;
     var kq = 1;
     for (var i = 1; i <= giaithua; i++) {
-      kq *= i;
+        kq *= i;
     }
     console.log(kq);
 
-    
-    document.getElementById("giaithua").innerHTML = "Tổng:  "+ "\n" + Number(kq);
+
+    document.getElementById("giaithua").innerHTML = "Tổng:  " + "\n" + Number(kq);
 }
 document.getElementById("tinhgiaithua").onclick = tinhgiaithua;
 
 
 
 //  bai 7
-function timsonguyento(so)
+function timsonguyento(x) {
+    
+    if (x < 1) return 0;
+    for (var i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i == 0) return 0;
+    }
+    return 1;
+}
+function insonguyento()
 {
-    var so = document.getElementById("").value;
-    // Biến cờ hiệu
-    var flag = true;
-
-    // Nếu n bé hơn 2 tức là không phải số nguyên tố
-    if (so < 2){
-        flag = false;
-    }
-    else if (so == 2){
-        flag = true;
-    }
-    else if (so % 2 == 0){
-      flag = false;
-    }
-    else{
-        // lặp từ 3 tới n-1 với bước nhảy là 2 (i+=2)
-        for (var i = 3; i < Math.sqrt(so); i+=2)
-        {
-            if (so % i == 0){
-                flag = false;
-                break;
-            }
+    
+    do{
+        var so = document.getElementById("n").value;
+        var snt ="";
+    }while(so<1);
+    for(var j=2; j<=so;j++){
+        var kt = timsonguyento(j);
+        if(kt){
+            snt +=j + ",";
+            document.getElementById("songuyento").innerHTML =  "Số nguyên tố" +"\n" +snt;
+            console.log(Number(snt))
         }
     }
 
-    // Kiểm tra biến flag
-    if (flag == true){
-        document.getElementById("songuyento").innerHTML = Number(so);
-    }
-    else{
-        document.getElementById("songuyento").innerHTML = "";
-    }
 }
-document.getElementById("insonguyento").onclick = timsonguyento;
+document.getElementById("insonguyento").onclick = insonguyento;
